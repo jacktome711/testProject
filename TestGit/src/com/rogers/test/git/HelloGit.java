0923ebb82a14dc44d11c822bcfe980424d220224
jacktome711 <jacktome@gmail.com>
2017-04-01 15:40:53 -0400

    done

--- TestGit/src/com/rogers/test/git/HelloGit.java
+++ TestGit/src/com/rogers/test/git/HelloGit.java
@@ -3,7 +3,6 @@ package com.rogers.test.git;
 public class HelloGit {
 
 	public static void main(String[] args) {
-		
 		System.out.println("Hello there");
 		System.out.println("tggello tffffhere2");
 	}
